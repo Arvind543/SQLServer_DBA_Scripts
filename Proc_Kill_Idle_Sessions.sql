235b294fccca384d93f862d883da0be42177d6b5
Arvind Toorpu <33610858+Arvind543@users.noreply.github.com>
2024-12-04 10:01:12 -0600

    Update Proc_Kill_Idle_Sessions.sql

--- Proc_Kill_Idle_Sessions.sql
+++ Proc_Kill_Idle_Sessions.sql
@@ -1,121 +1,76 @@
 -----  Below is a stored procedure for SQL Server that identifies and kills sessions (SPIDs) that have been idle for more than one day.
 ----- Procedure to Kill Idle Sessions Older Than One Day
-
-
-
-CREATE PROCEDURE Kill_Idle_Sessions
-
+CREATE PROCEDURE kill_idle_sessions
 AS
-
-BEGIN
-
-    SET NOCOUNT ON;
-
-
-
+  BEGIN
+    SET nocount ON;
     -- Temporary table to hold idle sessions
-
-    CREATE TABLE #IdleSessions (
-
-        SPID INT,
-
-        Status NVARCHAR(50),
-
-        LoginName NVARCHAR(256),
-
-        LastBatch DATETIME
-
-    );
-
-
-
+    CREATE TABLE #idlesessions
+                 (
+                              spid      INT,
+                              status    NVARCHAR(50),
+                              loginname NVARCHAR(256),
+                              lastbatch DATETIME
+                 );
+    
     -- Insert idle sessions into the temporary table
-
-    INSERT INTO #IdleSessions (SPID, Status, LoginName, LastBatch)
-
-    SELECT spid, status, loginame, last_batch
-
-    FROM sys.sysprocesses
-
-    WHERE status = 'sleeping' -- Idle sessions
-
-      AND DATEDIFF(DAY, last_batch, GETDATE()) > 1; -- Idle for more than 1 day
-
-
-
+    INSERT INTO #idlesessions
+                (
+                            spid,
+                            status,
+                            loginname,
+                            lastbatch
+                )
+    SELECT spid,
+           status,
+           loginame,
+           last_batch
+    FROM   sys.sysprocesses
+    WHERE  status = 'sleeping'                       -- Idle sessions
+    AND    Datediff(day, last_batch, Getdate()) > 1; -- Idle for more than 1 day
     -- Loop through the sessions and kill them
-
-    DECLARE @SPID INT, @Status NVARCHAR(50), @LoginName NVARCHAR(256), @LastBatch DATETIME;
-
-
-
-    DECLARE IdleCursor CURSOR FOR
-
-        SELECT SPID, Status, LoginName, LastBatch
-
-        FROM #IdleSessions;
-
-
-
-    OPEN IdleCursor;
-
-
-
-    FETCH NEXT FROM IdleCursor INTO @SPID, @Status, @LoginName, @LastBatch;
-
-
-
+    DECLARE @SPID INT,
+      @Status     NVARCHAR(50),
+      @LoginName  NVARCHAR(256),
+      @LastBatch  DATETIME;
+    DECLARE idlecursor CURSOR FOR
+    SELECT spid,
+           status,
+           loginname,
+           lastbatch
+    FROM   #idlesessions;
+    
+    OPEN idlecursor;
+    FETCH next
+    FROM  idlecursor
+    INTO  @SPID,
+          @Status,
+          @LoginName,
+          @LastBatch;
+    
     WHILE @@FETCH_STATUS = 0
-
     BEGIN
-
-        PRINT 'Killing SPID: ' + CAST(@SPID AS NVARCHAR) +
-
-              ', LoginName: ' + @LoginName +
-
-              ', LastBatch: ' + CAST(@LastBatch AS NVARCHAR);
-
-
-
-        BEGIN TRY
-
-            -- Kill the session
-
-            EXEC('KILL ' + CAST(@SPID AS NVARCHAR));
-
-        END TRY
-
-        BEGIN CATCH
-
-            PRINT 'Error occurred while killing SPID: ' + CAST(@SPID AS NVARCHAR) + ' - ' + ERROR_MESSAGE();
-
-        END CATCH;
-
-
-
-        FETCH NEXT FROM IdleCursor INTO @SPID, @Status, @LoginName, @LastBatch;
-
+      PRINT 'Killing SPID: ' + Cast(@SPID AS NVARCHAR) + ', LoginName: ' + @LoginName + ', LastBatch: ' + Cast(@LastBatch AS NVARCHAR);
+      BEGIN try
+        -- Kill the session
+        EXEC('KILL ' + cast(@SPID AS nvarchar));
+      END try
+      BEGIN catch
+        PRINT 'Error occurred while killing SPID: ' + Cast(@SPID AS NVARCHAR) + ' - ' + Error_message();
+      END catch;
+      FETCH next
+      FROM  idlecursor
+      INTO  @SPID,
+            @Status,
+            @LoginName,
+            @LastBatch;
+    
     END;
-
-
-
-    CLOSE IdleCursor;
-
-    DEALLOCATE IdleCursor;
-
-
-
+    CLOSE idlecursor;
+    DEALLOCATE idlecursor;
     -- Clean up the temporary table
-
-    DROP TABLE #IdleSessions;
-
-
-
+    DROP TABLE #idlesessions;
+    
     PRINT 'Idle session cleanup complete.';
-
-END;
-
+  END;
 GO
-
-
-
